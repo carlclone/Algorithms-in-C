cada81e9f2d4469f41b71da22ef8fb7470169fa8
carlclone <906561974@qq.com>
2020-07-18 11:11:30 +0800

    Update merge-two-binary-trees.c

--- merge-two-binary-trees.c
+++ merge-two-binary-trees.c
@@ -9,6 +9,7 @@ struct TreeNode* mergeTrees(struct TreeNode* t1, struct TreeNode* t2){
     
     t1->val +=t2->val;
     
+    //树的题利用好返回值
     t1->left = mergeTrees(t1->left,t2->left);
     t1->right = mergeTrees(t1->right,t2->right);
     
